ea8cdca4916cb153d3e393eee0de69de73bf2bae
Alejandro Franco <jafrancov@gmail.com>
2020-07-17 18:29:50 -0500

    Quotes App

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:first_flutter_app/ui/home.dart';
 
 void main() => runApp(new MaterialApp(
-      home: BizCard(),
+      home: Wisdom(),
+//      home: BizCard(),
 //      home: ScaffoldExample(),
     ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,102 @@
 import 'package:flutter/material.dart';
 
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
+
+class _WisdomState extends State<Wisdom> {
+  int _index = 0;
+  List quotes = [
+    'La innovación distingue a los líderes de los seguidores.',
+    'Muchas veces la gente no sabe lo que quiere hasta que se lo enseñas.',
+    'La mitad de lo que separa a los emprendedores exitosos de los que no triunfan es la perseverancia.',
+    'La vida es injusta, acostúmbrate a ello.',
+    'Está bien celebrar el éxito, pero es más importante prestar atención a las lecciones del fracaso.',
+    'Las grandes oportunidades nacen de haber sabido aprovechar las pequeñas.',
+    'La vida es frágil. No tenemos garantizado un mañana, así que da lo mejor de ti.',
+    'Mi filosofía siempre ha sido ser el mejor, no ser el primero.',
+    'Puedes centrarte en las barreras o bien en escalar el muro y redefinir el problema.',
+    'Cuando un sueño aparezca, ¡agárralo!',
+    'Debes hacer cosas que realmente sean importantes, pero también debes divertirte, porque si no, no tendrás éxito.',
+    'Sé que parece que el mundo se está desmoronando, pero en realidad es una gran época para volvernos un poco locos, seguir nuestra curiosidad y ser ambiciosos. No abandonéis vuestros sueños. ¡El mundo os necesita!',
+    'Si decides hacer solo las cosas que sabes que van a funcionar, dejarás un montón de oportunidades encima de la mesa.',
+    'Tu margen es mi oportunidad.',
+    'El único modo de salir de una caja cerrada es inventar la manera de salir.',
+    'El mayor riesgo es no correr ningún riesgo.',
+    'Encuentra aquello por lo que realmente sientes pasión.',
+    'La pregunta no es "¿qué queremos saber de la gente?", sino "¿qué desea contarnos la gente de sí misma?"',
+    'El mundo se puede cambiar en 140 caracteres.',
+    'No importa tanto a cuántas personas sigas, ni el número de seguidores que tengas, sino lo que pones en cada tweet.',
+    'Twitter es como la electricidad. Puedes usarlo cuando quieras.',
+    'Creo que este es el mejor consejo: piensa constantemente cómo podrías hacer mejor las cosas.',
+    'Si algo es lo suficientemente importante, deberías intentarlo. Incluso si el resultado probable es el fracaso.',
+    'Creo que es posible que la gente normal elija ser extraordinaria.',
+    'El marketing realmente brillante es aquel que toma algo que la mayoría interpreta como una debilidad y lo transforma para que sea visto como una fortaleza.',
+    'La mayoría de ideas emprendedoras pueden sonar alocadas, estúpidas y poco rentables, pero luego serán un éxito.',
+    'No tengas miedo de cambiar el modelo.',
+    'Cuando empieces a construir un producto, no asumas que sabes todas las respuestas. Escucha a la comunidad y adáptate a ella.',
+    'El éxito de cualquier producto depende de visiones muy sutiles. Necesitas un gran producto y una visión más grande todavía para llevarlo a cabo, pero serán las cosas pequeñas las que marquen la gran diferencia.',
+    'En cierto modo, ser un emprendedor es un viaje solitario.',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Container(
+                    width: 350,
+                    height: 200,
+                    margin: EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(14.5)),
+                    child: Center(
+                        child: Text(
+                      quotes[_index % quotes.length],
+                      style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 16.5,
+                          fontStyle: FontStyle.italic),
+                    ))),
+              ),
+            ),
+            Divider(
+              thickness: 1.3,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: FlatButton.icon(
+                  onPressed: _showQuote,
+                  color: Colors.greenAccent.shade700,
+                  icon: Icon(
+                    Icons.wb_sunny,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Inspire me!',
+                    style: TextStyle(color: Colors.white, fontSize: 18.8),
+                  )),
+            ),
+            Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showQuote() {
+    setState(() {
+      _index += 1;
+    });
+  }
+}
+
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,20 +124,33 @@ class BizCard extends StatelessWidget {
       height: 200,
       margin: EdgeInsets.all(50.0),
       decoration: BoxDecoration(
-        color: Colors.lightGreen,
-        borderRadius: BorderRadius.circular(14.5)
-      ),
+          color: Colors.lightGreen, borderRadius: BorderRadius.circular(14.5)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text('Alejandro Franco', style: TextStyle(fontSize: 20.9, fontWeight: FontWeight.bold, color: Colors.white),),
-          SizedBox(height: 10.0,),
-          Text('alexfranco.dev', style: TextStyle(fontSize: 15.0, color: Colors.white),),
+          Text(
+            'Alejandro Franco',
+            style: TextStyle(
+                fontSize: 20.9,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'alexfranco.dev',
+            style: TextStyle(fontSize: 15.0, color: Colors.white),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(Icons.person_outline, color: Colors.white,),
-              Text('@jafrancov', style: TextStyle(fontSize: 15.0, color: Colors.white)),
+              Icon(
+                Icons.person_outline,
+                color: Colors.white,
+              ),
+              Text('@jafrancov',
+                  style: TextStyle(fontSize: 15.0, color: Colors.white)),
             ],
           )
         ],
@@ -53,18 +163,15 @@ class BizCard extends StatelessWidget {
       width: 100,
       height: 100,
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-        border: Border.all(color: Colors.white70, width: 1.2),
-        image: DecorationImage(
-            image: NetworkImage('https://picsum.photos/300/300'),
-            fit: BoxFit.cover
-        )
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(color: Colors.white70, width: 1.2),
+          image: DecorationImage(
+              image: NetworkImage('https://picsum.photos/300/300'),
+              fit: BoxFit.cover)),
     );
   }
 }
-
 
 class ScaffoldExample extends StatelessWidget {
   _tapButton() {
